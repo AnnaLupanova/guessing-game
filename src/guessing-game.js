b18aa3ee55cbef8555b1797311ce32363930fdca
Lupanova Anna <hanna.lupanava@newland.by>
2022-07-04 10:56:36 +0300

    done task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,25 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.rigth = max;
+        this.left = min;
     }
 
-    guess() {
 
+    guess() {
+        let middle = Math.ceil((this.rigth +  this.left)/2);
+        this.middle = middle;
+        return this.middle ;
     }
 
     lower() {
+        this.rigth = this.middle ;
+
 
     }
 
     greater() {
-
+        this.left = this.middle ;
     }
 }
-
 module.exports = GuessingGame;
